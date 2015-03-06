53e318ecec9835f0c15bf089be9f59ef1602dc2a
Kamilo Amir <kamilo@gmail.com>
2015-03-05 20:26:57 -0500

    Working Version 1.0
    Full functional version 1.0 of the TA.  This works in as a standalone,
    but still requires the Splunk App for AWS and the Splunk TA for AWS to
    work.

--- latest/Splunk_TA_dome9/bin/aws_config.py
+++ latest/Splunk_TA_dome9/bin/aws_config.py
@@ -1,5 +1,5 @@
 """
-Modular Input for AWS Dome9
+Modular Input for AWS Config
 """
 
 import sys
@@ -25,11 +25,11 @@ import boto.sqs
 import boto.sqs.jsonmessage
 import boto.s3.connection
 import boto.exception
-from boto.sqs.message import RawMessage
 
 import logging
 from taaws.log import setup_logger
-logger = setup_logger(os.path.basename(__file__), level=logging.DEBUG)
+logger = setup_logger(os.path.basename(__file__), level=logging.ERROR)
+from taaws.log_settings import get_level
 
 import taaws.s3util
 
@@ -54,8 +54,8 @@ class MyScript(smi.Script):
     def get_scheme(self):
         """overloaded splunklib modularinput method"""
 
-        scheme = smi.Scheme("AWS Dome9")
-        scheme.description = ("Collect notifications produced by AWS Dome9."
+        scheme = smi.Scheme("AWS Config")
+        scheme.description = ("Collect notifications produced by AWS Config."
                               "The feature must be enabled and its SNS topic must be subscribed to an SQS queue.")
         scheme.use_external_validation = True
         scheme.streaming_mode_xml = True
@@ -73,9 +73,9 @@ class MyScript(smi.Script):
                                                       " e.g., us-east-1, us-west-2, eu-west-1, ap-southeast-1, etc."),
                                          required_on_create=True, required_on_edit=True))
         scheme.add_argument(smi.Argument("sqs_queue", title="SQS Queue Name",
-                                         description=("Name of queue to which notifications of AWS Dome9"
+                                         description=("Name of queue to which notifications of AWS Config"
                                                       " are sent. The queue should be subscribed"
-                                                      " to the AWS Dome9 SNS topic."),
+                                                      " to the AWS Config SNS topic."),
                                          required_on_create=True, required_on_edit=True))
         scheme.add_argument(smi.Argument("enable_additional_notifications", title="Enable Debug",
                                          description=("Index additional SNS/SQS events to help with troubleshooting."),
@@ -152,6 +152,8 @@ class MyScript(smi.Script):
 
     def stream_events(self, inputs, ew):
         """overloaded splunklib modularinput method"""
+        logger.setLevel(get_level(os.path.basename(__file__)[:-3],self.service.token, appName=APPNAME))
+        logger.log(logging.INFO, "STARTED: {}".format(len(sys.argv) > 1 and sys.argv[1] or ''))
         logger.log(logging.DEBUG, "Start streaming.")
         self._ew = ew
 
@@ -174,6 +176,7 @@ class MyScript(smi.Script):
 
         session_key = self.service.token
         aws_account_name = self.input_items.get("aws_account") or "default"
+        base_sourcetype = self.input_items.get("sourcetype") or "aws:config"
         (key_id, secret_key) = self.get_access_key_pwd_real(session_key=session_key, aws_account_name=aws_account_name)
 
         # Try S3 Connection
@@ -213,8 +216,7 @@ class MyScript(smi.Script):
                             self.input_items['sqs_queue']))
                         raise
 
-                #sqs_queue.set_message_class(boto.sqs.message.RawMessage)
-                sqs_queue.set_message_class(RawMessage)
+                sqs_queue.set_message_class(boto.sqs.message.RawMessage)
 
                 # num_messages=10 was chosen based on aws pricing faq.
                 # see request batch pricing: http://aws.amazon.com/sqs/pricing/
@@ -228,6 +230,7 @@ class MyScript(smi.Script):
                 stats = {'written': 0}
 
                 # if not notifications or self._canceled:
+                #     continue
 
                 # Exit if SQS returns nothing. Wake up on interval as specified on inputs.conf
                 if len(notifications) == 0:
@@ -238,19 +241,92 @@ class MyScript(smi.Script):
                     if self._canceled:
                         break
                     try:
- 			message = notification.get_body()
-			logger.log(logging.DEBUG, message)	
-			event=smi.Event((message),
-				source="aws:dome9:notification")
-			ew.write_event(event)
-                        stats['written'] += 1
-			completed.append(notification)
-
-                    # What do we do with non JSON data? Leave them in the queue but recommend customer uses a SQS queue only for AWS Dome9?
+                        envelope = json.loads(notification.get_body())
+                    # What do we do with non JSON data? Leave them in the queue but recommend customer uses a SQS queue only for AWS Config?
                     except Exception as e:
                         failed.append(notification)
-                        logger.log(logging.ERROR, "Problems decoding JSON in notification: {} {}.".format(
+                        logger.log(logging.ERROR, "problems decoding notification JSON string: {} {}".format(
                             type(e).__name__, e))
+                        continue
+
+                    if not isinstance(envelope,dict):
+                        failed.append(notification)
+                        logger.log(logging.ERROR, "This doesn't look like a valid Config message. Please check SQS settings.")
+                        continue
+
+                    if all(key in envelope for key in ("Type", "MessageId", "TopicArn", "Message")) and isinstance(envelope['Message'],basestring):
+                        logger.log(logging.DEBUG, "This is considered a Config notification.")
+                        try:
+                            envelope = json.loads(envelope['Message'])
+                            if not isinstance(envelope,dict):
+                                failed.append(notification)
+                                logger.log(logging.ERROR, "This doesn't look like a valid Config message. Please check SQS settings.")
+                                continue
+                        except Exception as e:
+                            failed.append(notification)
+                            logger.log(logging.ERROR, "problems decoding message JSON string: {} {}".format(
+                                type(e).__name__, e))
+                            continue
+
+
+                    if 'messageType' in envelope:
+                        logger.log(logging.DEBUG, "This is considered a Config message. 'Raw Message Delivery' may be 'True'.")
+                        message=envelope
+                    else:
+                        failed.append(notification)
+                        logger.log(logging.ERROR, "This doesn't look like a valid Config message. Please check SQS settings.")
+                        continue
+
+
+
+                    ## Process: config notifications, history and snapshot notifications (additional)
+
+                    # Process notifications with payload, check ConfigurationItemChangeNotification
+                    msg_type=message.get('messageType', '')
+                    if msg_type == 'ConfigurationItemChangeNotification':
+                        logger.log(logging.DEBUG, "Consuming configuration change data in SQS payload.")
+                        # determine _time for the event
+                        configurationItem = message.get('configurationItem', '')
+                        configurationItemCaptureTime= configurationItem.get('configurationItemCaptureTime', '')
+                        event_time = int(calendar.timegm(time.strptime(configurationItemCaptureTime.replace("Z", "GMT"), "%Y-%m-%dT%H:%M:%S.%f%Z")))
+                        # write the event
+                        event = smi.Event(data=json.dumps(message),
+                                      time=event_time,
+                                      sourcetype=base_sourcetype+":notification")
+                        ew.write_event(event)
+                        stats['written'] += 1
+                        completed.append(notification)
+
+                    # Process ConfigurationHistoryDeliveryCompleted notifications by fetching data from S3 buckets
+                    elif msg_type == 'ConfigurationHistoryDeliveryCompleted' and message.get('s3ObjectKey', '') != '' and message.get('s3Bucket', '') != '' :
+                        logger.log(logging.DEBUG, "Consuming configuration history change data in S3 bucket.")
+                        # hardcode bucket_name for now
+                        # key = message['s3FileLocation']
+                        bucket_name = message.get('s3Bucket', '')
+                        key = message.get('s3ObjectKey', '')
+
+                        completed_buf, failed_buf = self.process_confighistory(s3_conn, notification, bucket_name, key)
+                        completed.extend(completed_buf)
+                        failed.extend(failed_buf)
+                        logger.log(logging.DEBUG, "Length of completed after reaching into s3bucket:", len(completed))
+
+
+                    # Ingest all other notification of types: ConfigurationSnapshot*etc. but only when enable_additional_notifications is true.
+                    elif self.enable_additional_notifications and msg_type.startswith("ConfigurationSnapshot"):
+                        logger.log(logging.DEBUG, "Consuming additional notifications enabled")
+                        notificationCreationTime = message.get('notificationCreationTime', '')
+                        event_time = int(calendar.timegm(time.strptime(notificationCreationTime.replace("Z", "GMT"), "%Y-%m-%dT%H:%M:%S.%f%Z")))
+                        # write the event
+                        event = smi.Event(data=json.dumps(message),
+                                      time=event_time,
+                                      source=base_sourcetype+":additional")
+                        ew.write_event(event)
+                        stats['written'] += 1
+                        completed.append(notification)
+
+                    else:
+                        failed.append(notification)
+                        logger.log(logging.ERROR, "This doesn't look like a Config notification or message. Please check SQS settings.")
                         continue
 
                 notification_delete_errors = 0
@@ -259,6 +335,15 @@ class MyScript(smi.Script):
                     br = sqs_queue.delete_message_batch(completed)
                     if br.errors:
                         notification_delete_errors = len(br.errors)
+
+                if failed:
+                    logger.log(logging.DEBUG, "sqs_queue.delete_message_batch(failed)")
+                    br = sqs_queue.delete_message_batch(failed)
+                    logger.log(logging.DEBUG, "sqs_queue.delete_message_batch done")
+                    if br.errors:
+                        notification_delete_errors = len(br.errors)
+                    failed_messages = ','.join([ m.get_body() for m in failed])
+                    logger.log(logging.WARN, "Invalid notifications have been removed from SQS : %s", failed_messages)
 
                 else:
                     logger.log(logging.INFO, ("{} completed, {} failed while processing a notification batch of {}"
@@ -271,9 +356,99 @@ class MyScript(smi.Script):
             logger.log(logging.FATAL, "Outer catchall: %s: %s", type(e).__name__, e)
             raise
 
+
+    def process_confighistory(self, s3_conn, notification, bucket_name, key):
+        """Extract events from AWS Config S3 logs referenced in SNS notifications."""
+
+        completed = []
+        failed = []
+
+        try:
+            # defer validation to minimize queries.
+            s3_bucket = s3_conn.get_bucket(bucket_name, validate=False)
+
+            s3_file = s3_bucket.get_key(key)
+            if s3_file is None:
+                file_json = {}
+            else:
+                with io.BytesIO(s3_file.read()) as bio:
+                    with gzip.GzipFile(fileobj=bio) as gz:
+                        file_json = json.loads(gz.read())
+
+        except boto.exception.S3ResponseError as e:
+
+                # TODO: if e.error_code == 'NoSuchBucket' --- should we delete from queue also?
+                # Or is this something that should be left for SQS Redrive?
+
+                loglevel = logging.ERROR
+                if e.status == 404 and e.reason == 'Not Found' and e.error_code in ('NoSuchKey',):
+                    completed.append(notification)
+                    loglevel = logging.WARN
+                else:
+                    failed.append(notification)
+
+                edetail = e.body
+                if e.body:
+                    try:
+                        elem = ET.fromstring(e.body)
+                        edetail = elem.findtext('Key') or elem.findtext('BucketName') or ''
+                    except Exception:
+                        logger.log(logging.WARN,"Failed to parse the content from S3ResponseError : {}".format(e.body))
+
+                logger.log(loglevel, "{}: {} {}: {} - {}: {}".format(
+                    type(e).__name__, e.status, e.reason, e.error_code, e.error_message, edetail))
+
+        except ValueError as e:
+            failed.append(notification)
+            logger.log(logging.ERROR, "Problems reading json from s3:{}/{}: {} {}".format(
+                bucket_name, key, type(e).__name__, e))
+
+        except IOError as e:
+            failed.append(notification)
+            logger.log(logging.ERROR, "Problems unzipping from s3:{}/{}: {} {}".format(
+                bucket_name, key, type(e).__name__, e))
+
+        try:
+            configurationItems = file_json.get('configurationItems', [])
+            logger.log(logging.INFO, "Processing {} configurationItems in s3:{}/{}".format(
+                len(configurationItems), bucket_name, key))
+        except KeyError as e:
+            failed.append(notification)
+            logger.log(logging.ERROR, "JSON not in expected format from s3:{}/{}: {} {}".format(
+                bucket_name, key, type(e).__name__, e))
+
+        stats = {'written': 0}
+
+
+        source = os.path.basename(key)
+
+        # Extract payload elements from history files
+
+        try:
+            for configurationItem in configurationItems:
+                configurationItemCaptureTime = configurationItem.get('configurationItemCaptureTime', '')
+                event_time = int(calendar.timegm(time.strptime(configurationItemCaptureTime.replace("Z", "GMT"), "%Y-%m-%dT%H:%M:%S.%f%Z")))
+                #write the event
+                event = smi.Event(data=json.dumps(configurationItem),
+                                  time=event_time,
+                                  source=source)
+                self._ew.write_event(event)
+                stats['written'] += 1
+
+
+            logger.log(logging.INFO, ("Fetched {} configurationItems, wrote {}"
+                                      " from s3:{}/{}").format(len(configurationItems), stats['written'], bucket_name, key))
+            completed.append(notification)
+
+        except IOError as e:
+            if not self._canceled:
+                failed.append(notification)
+
+        return completed, failed
+
+
+
 if __name__ == "__main__":
 
-    logger.log(logging.INFO, "STARTED: {}".format(len(sys.argv) > 1 and sys.argv[1] or ''))
     exitcode = MyScript().run(sys.argv)
-    logger.log(logging.INFO, "EXITED: {}".format(exitcode))
     sys.exit(exitcode)
